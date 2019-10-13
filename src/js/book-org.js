5dbb9b69a1cfcda04ddbd9acf90298a60053d5fa
evgenynovak <evnovak171@mail.ru>
2019-10-13 20:17:36 +0300

    add teg in book list

--- src/js/book-org.js
+++ src/js/book-org.js
@@ -1,17 +1,37 @@
 
 const bookForm = document.querySelector('.book-form');
 const bookFormAdd = document.querySelector('.book-form__add');
+const bookFormRadioInputs = document.querySelectorAll('.book-form__radio-input');
 const bookList = document.querySelector('.book-list'); 
 const bookListItems = document.querySelectorAll('.book-list__item');
 
-function addBookListItem(event){
-  event.preventDefault();
-  const bookListItem = createListItem(bookFormAdd.value);
-  bookList.appendChild(bookListItem);
-  bookFormAdd.value = '';
+function getTeg(allTegs) {
+  bookTegs = document.createElement('span');
+  bookTegs.className = 'book-list__tegs';
+  for (let i = 0; i < allTegs.length ; i++) {
+    if ( allTegs[i].checked ) {
+      bookTeg = document.createElement('label');
+      bookTeg.innerText = allTegs[i].value;
+      bookTeg.className = 'book-list__teg teg';
+      bookTegs.appendChild(bookTeg);
+    }
+  };
+  
+  return bookTegs
 };
 
-function createListItem(bookName){
+function addBookListItem(event){
+  event.preventDefault();
+  const bookListItem = createListItem(bookFormAdd.value, getTeg(bookFormRadioInputs));
+  bookList.appendChild(bookListItem);
+  // чистим форму
+  bookFormAdd.value = '';
+  for (let i = 0; i < bookFormRadioInputs.length ; i++) {
+    bookFormRadioInputs[i].checked = false;
+  };
+};
+
+function createListItem(bookName, tags){
 
   const ListItem = document.createElement('li');
   ListItem.className = 'book-list__item';
@@ -29,6 +49,7 @@ function createListItem(bookName){
   buttonDelete.className = ' book-list__button book-list__button_delete';
 
   ListItem.appendChild(bookListTitle);
+  ListItem.appendChild(tags);
   ListItem.appendChild(buttonEdit);
   ListItem.appendChild(buttonDelete);
   
@@ -37,6 +58,7 @@ function createListItem(bookName){
 
 
 bookForm.addEventListener('submit', addBookListItem);
+
 
 
 
