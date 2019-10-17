cc8216655663a97f8044d84fd91057573dbf6972
evgenynovak <evnovak171@mail.ru>
2019-10-18 02:23:48 +0300

    Tegs: adding and removing tags in item

--- src/js/book-org.js
+++ src/js/book-org.js
@@ -4,27 +4,47 @@ const bookFormAdd = document.querySelector('.book-form__add');
 const bookFormRadioInputs = document.querySelectorAll('.book-form__radio-input');
 const bookList = document.querySelector('.book-list'); 
 const bookListItems = document.querySelectorAll('.book-list__item');
+let itemId = 0;
 
-function getTeg(allTegs) {
-  bookTegs = document.createElement('span');
-  bookTegs.className = 'book-list__tegs';
-  for (let i = 0; i < allTegs.length ; i++) {
-    if ( allTegs[i].checked ) {
-      bookTeg = document.createElement('label');
-      bookTeg.innerText = allTegs[i].value;
-      bookTeg.className = 'book-list__teg teg';
-      bookTegs.appendChild(bookTeg);
+function selectedTags(allTags) {
+  booktags = document.createElement('span');
+  booktags.className = 'book-list__tags';
+  for (let i = 0; i < allTags.length ; i++) {
+    if ( allTags[i].checked ) {
+      booktag = document.createElement('label');
+      booktag.innerText = allTags[i].value;
+      booktag.className = 'book-list__tag tag';
+      booktags.append(booktag);
     }
   };
-  
-  return bookTegs
+  return booktags
+};
+
+function tagsAll(tags, tagsId) {
+  let fragment = new DocumentFragment();
+  for (let i = 0; i < tags.length ; i++) {
+      bookInput = document.createElement('input');
+      bookInput.className = 'book-list__radio-input';
+      bookInput.id = tags[i].id + tagsId;
+      bookInput.type = tags[i].type;
+      bookInput.value = tags[i].value;
+      bookInput.name = tags[i].name;
+      bookInput.checked = tags[i].checked;
+      booktag = document.createElement('label');
+      booktag.innerText = tags[i].value;
+      booktag.className = 'book-list__tag tag';
+      booktag.prepend(bookInput);
+      fragment.append(booktag);
+  };
+  return fragment
 };
 
 
 
 function addBookListItem(event){
   event.preventDefault();
-  const bookListItem = createListItem(bookFormAdd.value, getTeg(bookFormRadioInputs));
+  itemId = ++itemId
+  const bookListItem = createListItem(bookFormAdd.value, bookFormRadioInputs, itemId);
   bookList.appendChild(bookListItem);
   // чистим форму
   bookFormAdd.value = '';
@@ -33,10 +53,16 @@ function addBookListItem(event){
   };
 };
 
-function createListItem(bookName, tags){
+function createListItem(bookName, tags, id){
 
   const ListItem = document.createElement('li');
   ListItem.className = 'book-list__item';
+
+  const bookLictBlock = document.createElement('div');
+  bookLictBlock.className = 'book-list__block';
+
+  const bookLictBlockAfter = bookLictBlock.cloneNode(false);
+  bookLictBlockAfter.classList.add('book-list__block_hide');
   
   const bookListTitle = document.createElement('label');
   bookListTitle.innerText = bookName;
@@ -53,11 +79,15 @@ function createListItem(bookName, tags){
   buttonDelete.innerText = 'Удалить';
   buttonDelete.className = ' book-list__button book-list__button_delete';
 
-  ListItem.appendChild(bookListTitle);
-  ListItem.appendChild(bookListTitleInput);
-  ListItem.appendChild(tags);
-  ListItem.appendChild(buttonEdit);
-  ListItem.appendChild(buttonDelete);
+  ListItem.append(bookLictBlock);
+  ListItem.append(bookLictBlockAfter);
+  bookLictBlockAfter.append(tagsAll(tags,id));
+  bookLictBlock.after(bookLictBlock);
+  bookLictBlock.append(bookListTitle);
+  bookLictBlock.append(bookListTitleInput);
+  bookLictBlock.append(selectedTags(tags));
+  bookLictBlock.append(buttonEdit);
+  bookLictBlock.append(buttonDelete);
 
   buttonEdit.addEventListener('click', editBookItem);
   buttonDelete.addEventListener('click', deleteBookItem);
@@ -66,25 +96,31 @@ function createListItem(bookName, tags){
 };
 
 function editBookItem(){
-  bookItem =  this.parentNode;
-  title = bookItem.querySelector('.book-list__title');
-  titleInput = bookItem.querySelector('.book-list__title-input');
-  isEdit = bookItem.classList.contains('book-list__item_edit');
+  const bookItem =  this.parentNode.parentNode;
+  const title = bookItem.querySelector('.book-list__title');
+  const titleInput = bookItem.querySelector('.book-list__title-input');
+  const tagsInput = bookItem.querySelector('.book-list__tags');
+  const isEdit = bookItem.classList.contains('book-list__item_edit');
+  const block = bookItem.querySelector('.book-list__block_hide');
+  const tagsAll = block.querySelectorAll('.book-list__radio-input');
 
   if ( isEdit ) {
     title.innerText = titleInput.value;
+    tagsInput.replaceWith(selectedTags(tagsAll));
     this.innerText = 'Изменить';
     } else {
     titleInput.value = title.innerText;
     this.innerText = 'Сохранить';
     };
 
+
+
     bookItem.classList.toggle('book-list__item_edit');
 
 };
 
 function deleteBookItem(){
-  bookList.removeChild(this.parentNode);
+  bookList.removeChild(this.parentNode.parentNode);
 };
 
 
